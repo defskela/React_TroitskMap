ab6518d7e0ae7521361f4311eb475e1d8f5235ab
defskela <sashokafanas@gmail.com>
2024-07-08 12:36:31 +0300

    Пока что не работает открытие модального окна при клике

--- src/components/ZoomableCanvas/ZoomableCanvas.jsx
+++ src/components/ZoomableCanvas/ZoomableCanvas.jsx
@@ -1,32 +1,43 @@
 import { fabric } from 'fabric'
 import { FabricJSCanvas, useFabricJSEditor } from 'fabricjs-react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { CardElement } from '../CardElement/CardElement'
+import reactImage from './react.svg' // Импорт изображения
+import { textIFWD } from './texts.jsx'
 
 export const ZoomableCanvas = () => {
     const { editor, onReady } = useFabricJSEditor()
+    const [open, setOpen] = useState(false)
 
     useEffect(() => {
         if (!editor) return
 
         const canvas = editor.canvas
+        canvas.setWidth(800) // Ширина холста
+        canvas.setHeight(600) // Высота холста
 
-        // Добавление объекта для демонстрации
-        canvas.add(
-            new fabric.Rect({
-                left: 100,
-                top: 100,
-                fill: 'red',
-                width: 20,
-                height: 20,
-            })
-        )
+        function addToCanvas(left, top, path) {
+            // Добавление объекта для демонстрации
+            fabric.Image.fromURL(path, function (img) {
+                // Можно задать начальные параметры для изображения
+                img.set({
+                    left: left,
+                    top: top,
+                    angle: 0,
+                    opacity: 4.0,
+                    hasControls: false,
+                    selectable: true, // Делает изображение невыбираемым и, следовательно, неперемещаемым
+                    // или
+                    lockMovementX: true,
+                    lockMovementY: true, // Блокирует перемещение по обеим осям
+                    hoverCursor: 'pointer',
+                })
 
-        canvas.forEachObject(function (obj) {
-            obj.set({
-                hasControls: false,
-                hoverCursor: 'pointer',
+                canvas.add(img)
             })
-        })
+        }
+
+        const fstImg = addToCanvas(400, 400, reactImage)
 
         // Обработка события прокрутки для масштабирования
         const handleMouseWheel = (opt) => {
@@ -42,11 +53,14 @@ export const ZoomableCanvas = () => {
 
         canvas.on('mouse:wheel', handleMouseWheel)
 
-        canvas.on('mouse:down', function (opt) {
-            opt.e.preventDefault()
-            opt.e.stopPropagation()
-            console.log('down')
-        })
+        // canvas.on('mouse:down', function (opt) {
+        //     opt.e.preventDefault()
+        //     opt.e.stopPropagation()
+        //     if (opt.target == fstImg) {
+        //         console.log('fstImg')
+        //     }
+        //     console.log('down')
+        // })
 
         // Очистка обработчиков
         return () => {
@@ -58,6 +72,20 @@ export const ZoomableCanvas = () => {
     return (
         <div>
             <FabricJSCanvas className="sample-canvas" onReady={onReady} />
+            <CardElement
+                open={open}
+                setOpen={setOpen}
+                header={'lorem'}
+                images={[
+                    'src/assets/ИФВД1.jpg',
+                    'src/assets/ИФВД2.jpg',
+                    'src/assets/ИФВД3.jpg',
+                ]}
+                hrefMoreDetails={'https://www.google.com/'}
+            >
+                {/* тексты хранятся в файле texts.jsx, чтобы не засорять тут код */}
+                {textIFWD}
+            </CardElement>
         </div>
     )
 }
