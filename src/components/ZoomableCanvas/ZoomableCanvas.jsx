416fcfbc18802f345d5a79f94a3096303a19f1bd
defskela <sashokafanas@gmail.com>
2024-07-30 14:42:03 +0300

    поправил передвижение курсором. Работаю над не выходом за границы

--- src/components/ZoomableCanvas/ZoomableCanvas.jsx
+++ src/components/ZoomableCanvas/ZoomableCanvas.jsx
@@ -14,8 +14,8 @@ export const ZoomableCanvas = () => {
         if (!editor) return
 
         const canvas = editor.canvas
-        canvas.setWidth(window.innerWidth) // Ширина холста
-        canvas.setHeight(window.innerHeight) // Высота холста
+        canvas.setWidth(10000 / 3) // Ширина холста
+        canvas.setHeight(5625 / 3) // Высота холста
 
         function addToCanvas(
             path,
@@ -60,12 +60,28 @@ export const ZoomableCanvas = () => {
                     canvas.on('mouse:move', function (opt) {
                         if (isDragging) {
                             const e = opt.e
-                            const vpt = this.viewportTransform
-                            vpt[4] += e.clientX - lastPosX
-                            vpt[5] += e.clientY - lastPosY
+                            // if (
+                            //     (img.left += e.clientX - lastPosX) < 0 &&
+                            //     (img.left += e.clientX - lastPosX) >
+                            // )
+                            img.left +=
+                                (e.clientX - lastPosX) / canvas.getZoom()
+                            img.top += (e.clientY - lastPosY) / canvas.getZoom()
                             this.requestRenderAll()
                             lastPosX = e.clientX
                             lastPosY = e.clientY
+                            // Предполагаем, что img - это объект изображения, добавленный на холст
+                            // Предполагаем, что img - это объект изображения, добавленный на холст
+
+                            console.log(
+                                'left',
+                                img.left,
+                                'top',
+                                img.top,
+                                'scale',
+                                canvas.getZoom()
+                            )
+
                             // Поставить ограничение, чтобы изображение не уходило за пределы холста
                         }
                     })
@@ -89,54 +105,12 @@ export const ZoomableCanvas = () => {
 
         // Обработка события прокрутки для масштабирования
         const handleMouseWheel = (opt) => {
-            var delta = opt.e.deltaY
-            console.log('delta', delta)
+            var scrollDelta = opt.e.deltaY
+            console.log('delta', scrollDelta)
             var zoom = canvas.getZoom()
-            zoom *= 0.999 ** delta
+            zoom *= 0.999 ** scrollDelta
             if (zoom > 20) zoom = 20
             if (zoom < 1) zoom = 1
-            if (delta > 0 && zoom > 1) {
-                console.log('delta > 0')
-                if (zoom > 1 && delta > 0) {
-                    // Рассчитываем коэффициент интерполяции
-                    // Начальное и конечное значения для интерполяции
-                    let interpolationFactor
-                    if (zoom > 1) {
-                        // Плавное изменение от zoom до 1
-                        interpolationFactor = (zoom - 1) / (20 - 1) // Максимальный зум равен 10
-                    } else {
-                        interpolationFactor = 1 // Когда zoom <= 1, интерполяция не требуется
-                    }
-                    console.log('interpolationFactor', interpolationFactor)
-
-                    // Получаем текущий viewportTransform
-                    let vpt = canvas.viewportTransform
-
-                    // Плавно обновляем vpt[4] и vpt[5] к 0, если zoom <= 1
-                    vpt[4] = vpt[4] * (1 - interpolationFactor)
-                    vpt[5] = vpt[5] * (1 - interpolationFactor)
-
-                    // Применяем обновленный viewportTransform
-                    canvas.setViewportTransform(vpt)
-
-                    console.log('zoom', zoom)
-                    canvas.zoomToPoint(
-                        { x: opt.e.offsetX, y: opt.e.offsetY },
-                        zoom
-                    )
-                    opt.e.preventDefault()
-                    opt.e.stopPropagation()
-                }
-            } else if (zoom == 1 && delta > 0) {
-                canvas.viewportTransform[4] = 0
-                canvas.viewportTransform[5] = 0
-            }
-            console.log(
-                'vpt',
-                canvas.viewportTransform[4],
-                canvas.viewportTransform[5]
-            )
-            console.log('zoom', zoom)
             canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom)
             opt.e.preventDefault()
             opt.e.stopPropagation()
